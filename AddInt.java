3b25245b6f2bd9123809397a93cb8d20ed6f1370
Omoope <adedoyinope@gmail.com>
2017-02-22 23:01:42 -0500

    AddInt.java

--- AddInt.java
+++ AddInt.java
@@ -2,8 +2,8 @@
 
 public class AddInt{
 	public static void main(String [] args){
-		int a = 12;
-		int b = 38;
+		double a = 12.0;
+		double b = 38.0;
 		double Avg = (a + b)/ 2.0;
 		System.out.println( Avg);
 
